9cfce9d043c066d58dd1d353023d56fdd15e6662
alexwnovak <alexwnovak@gmail.com>
2013-08-13 10:07:54 -0400

    Adding projects for EntryPoint and Engine. EntryPoint is currently a console app to hands it off to the MudServer class.

---
+++ Source/NewEnvy/NewEnvy.Engine/MudServer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewEnvy.Engine
+{
+   public class MudServer
+   {
+      public void Run()
+      {
+         Console.WriteLine( "Run" );
+      }
+   }
+}
---
+++ Source/NewEnvy/NewEnvy.EntryPoint/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NewEnvy.Engine;
+
+namespace NewEnvy.EntryPoint
+{
+   public class Program
+   {
+      public static void Main()
+      {
+         var mudServer = new MudServer();
+
+         mudServer.Run();
+      }
+   }
+}
