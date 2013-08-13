d7c7ef373fdc83210ac8c983a9873ee0db7c7ff7
alexwnovak <alexwnovak@gmail.com>
2013-08-13 10:48:54 -0400

    Pasting in TcpListener. Can accept one client and echo back what it receives.

---
+++ Source/NewEnvy/NewEnvy.Engine.Test/MudServerTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NewEnvy.Engine.Test
+{
+   [TestClass]
+   public class MudServerTest
+   {
+      [TestMethod]
+      public void Run_()
+      {
+         var mudServer = new MudServer();
+
+         mudServer.Run();
+      }
+   }
+}
