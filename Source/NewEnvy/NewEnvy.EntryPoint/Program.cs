d094909cac4257f9636a80fb2b22123aa5369b5a
alexwnovak <alexwnovak@gmail.com>
2013-08-15 21:18:06 -0400

    More code cleanup. Removed unused using directives, etc.

--- Source/NewEnvy/NewEnvy.EntryPoint/Program.cs
+++ Source/NewEnvy/NewEnvy.EntryPoint/Program.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using NewEnvy.Engine;
+﻿using NewEnvy.Engine;
 
 namespace NewEnvy.EntryPoint
 {
