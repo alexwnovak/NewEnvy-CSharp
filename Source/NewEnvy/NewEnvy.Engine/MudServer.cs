ed03f77e32e429ecaf407d7e58fe800207735160
alexwnovak <alexwnovak@gmail.com>
2013-08-18 12:23:36 -0400

    Cleanup on MudServer.cs

--- Source/NewEnvy/NewEnvy.Engine/MudServer.cs
+++ Source/NewEnvy/NewEnvy.Engine/MudServer.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using NewEnvy.Core;
+﻿using NewEnvy.Core;
 
 namespace NewEnvy.Engine
 {
@@ -27,61 +26,6 @@ namespace NewEnvy.Engine
 
             serverClock.EndClockAndWait();
          }
-         
-         //var tcpServer = Dependency.Resolve<IConnectionListener>();
-
-         //try
-         //{
-         //   tcpServer.StartAsync( _port );
-
-         //   var bytes = new byte[256];
-
-         //   while ( true )
-         //   {
-         //      Console.Write( "Waiting for a connection... " );
-
-         //      TcpClient client = tcpServer.Accept();
-
-         //      Console.WriteLine( "Connected!" );
-
-         //      NetworkStream stream = client.GetStream();
-
-         //      try
-         //      {
-         //         int bytesRead;
-
-         //         while ( ( bytesRead = stream.Read( bytes, 0, bytes.Length ) ) != 0 )
-         //         {
-         //            string data = System.Text.Encoding.ASCII.GetString( bytes, 0, bytesRead );
-         //            Console.WriteLine( "Received: {0}", data );
-
-         //            data = data.ToUpper();
-
-         //            byte[] msg = System.Text.Encoding.ASCII.GetBytes( data );
-
-         //            stream.Write( msg, 0, msg.Length );
-         //            Console.WriteLine( "Sent: {0}", data );
-         //         }
-
-         //         client.Close();
-         //      }
-         //      catch ( IOException )
-         //      {
-         //         client.Close();
-         //      }
-         //   }
-         //}
-         //catch ( SocketException e )
-         //{
-         //   Console.WriteLine( "SocketException: {0}", e );
-         //}
-         //finally
-         //{
-         //   tcpServer.Stop();
-         //}
-
-         //Console.WriteLine( "\nHit enter to continue..." );
-         //Console.Read();
       }
 
       public void Stop()
