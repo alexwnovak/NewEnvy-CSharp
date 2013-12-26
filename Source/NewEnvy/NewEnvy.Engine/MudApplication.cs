464bd3e9458f5838874e4b64cada748b1086bb25
Alex Novak <alexwnovak@gmail.com>
2013-12-26 13:26:11 -0500

    Moving DI stuff into Entry Point. Trimming MudApplication.

--- Source/NewEnvy/NewEnvy.Engine/MudApplication.cs
+++ Source/NewEnvy/NewEnvy.Engine/MudApplication.cs
@@ -1,28 +1,9 @@
-﻿using NewEnvy.Core;
-
-namespace NewEnvy.Engine
+﻿namespace NewEnvy.Engine
 {
    public class MudApplication
    {
-      private static void InitDependencyInjection()
-      {
-         Dependency.CreateUnityContainer();
-
-         Dependency.RegisterType<IConnectionListener, ConnectionListener>();
-         Dependency.RegisterType<IServerConfiguration, ServerConfiguration>();
-         Dependency.RegisterType<IDateTime, DateTimeAdapter>();
-
-         Dependency.RegisterInstance<IGlobalConnectionTable>( new GlobalConnectionTable() );
-         Dependency.RegisterInstance<IGlobalCommandQueue>( new GlobalCommandQueue() );
-         //Dependency.RegisterInstance( ClientManager.Instance );
-         //Dependency.RegisterInstance( GlobalCommandQueue.Instance );
-         //Dependency.RegisterType<IServerClock, ServerClock>();
-      }
-
       public void Main()
       {
-         InitDependencyInjection();
-
          var mudServer = new MudServer();
          mudServer.Run();
       }
--- Source/NewEnvy/NewEnvy.EntryPoint/Program.cs
+++ Source/NewEnvy/NewEnvy.EntryPoint/Program.cs
@@ -1,11 +1,29 @@
-﻿using NewEnvy.Engine;
+﻿using NewEnvy.Core;
+using NewEnvy.Engine;
 
 namespace NewEnvy.EntryPoint
 {
    public class Program
    {
+      private static void InitDependencyInjection()
+      {
+         Dependency.CreateUnityContainer();
+
+         Dependency.RegisterType<IConnectionListener, ConnectionListener>();
+         Dependency.RegisterType<IServerConfiguration, ServerConfiguration>();
+         Dependency.RegisterType<IDateTime, DateTimeAdapter>();
+
+         Dependency.RegisterInstance<IGlobalConnectionTable>( new GlobalConnectionTable() );
+         Dependency.RegisterInstance<IGlobalCommandQueue>( new GlobalCommandQueue() );
+         //Dependency.RegisterInstance( ClientManager.Instance );
+         //Dependency.RegisterInstance( GlobalCommandQueue.Instance );
+         //Dependency.RegisterType<IServerClock, ServerClock>();
+      }
+
       public static void Main()
       {
+         InitDependencyInjection();
+
          new MudApplication().Main();
       }
    }
