e519dd48ba79886cd2b2e690a70bac37d01aab40
Alex Novak <alexwnovak@gmail.com>
2013-12-26 14:30:00 -0500

    +Stub for first MudApplication unit test.

---
+++ Source/NewEnvy/NewEnvy.Engine.Test/MudApplicationTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NewEnvy.Core;
+
+namespace NewEnvy.Engine.Test
+{
+   [TestClass]
+   public class MudApplicationTest
+   {
+      [TestInitialize]
+      public void Initialize()
+      {
+         Dependency.CreateUnityContainer();
+      }
+
+      [TestMethod]
+      public void Start_()
+      {
+         var mudApplication = new MudApplication();
+
+         mudApplication.Start();
+      }
+   }
+}
